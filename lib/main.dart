f35b74277620362ba55315a584bc00d2a350171b
annika123 <annika15-6045@diu.edu.bd>
2025-02-26 14:36:41 +0600

    basic

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FoodApp());
+}
+
+class  FoodApp extends StatelessWidget {
+  const FoodApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
