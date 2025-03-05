09f5ce19e6c846ec754f653a2bfb7f6f45e90786
annika123 <annika15-6045@diu.edu.bd>
2025-03-05 11:41:30 +0600

    tab practice1

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,34 @@ void main() {
   runApp(const FoodApp());
 }
 
-class  FoodApp extends StatelessWidget {
+class FoodApp extends StatelessWidget {
   const FoodApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Food App"),
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.local_pizza), text: "Pizza"),
+                Tab(icon: Icon(Icons.fastfood), text: "Burger"),
+                Tab(icon: Icon(Icons.icecream), text: "Ice Cream"),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.local_pizza, size: 100, color: Colors.red),
+              Icon(Icons.fastfood, size: 100, color: Colors.orange),
+              Icon(Icons.icecream, size: 100, color: Colors.blue),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
